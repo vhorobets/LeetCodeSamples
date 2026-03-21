aadcf47edd3fdf2d945241fd16f965a605f50536
vhorobets <valikgor1@gmail.com>
2026-03-20 20:44:55 -0600

    Time: 0 ms (100.00%) | Memory: 13.3 MB (46.50%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        dict = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in dict:
+                return [dict[complement], i]
+
+            dict[nums[i]] = i
+        
