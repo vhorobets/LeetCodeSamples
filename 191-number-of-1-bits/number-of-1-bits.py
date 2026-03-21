e21026aa5484b76979fbc31c3db860aa7475be7a
vhorobets <valikgor1@gmail.com>
2026-03-20 21:13:32 -0600

    Time: 3 ms (19.65%) | Memory: 12.3 MB (76.70%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        count = 0
+
+        while n > 0:
+            count += n & 1
+            n = n >> 1
+
+        return count
+        
