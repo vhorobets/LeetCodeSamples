7af06f25ad8ee215798904add0e91b0dcf488ab6
vhorobets <valikgor1@gmail.com>
2026-04-02 13:15:18 -0600

    Time: 3 ms (21.55%) | Memory: 12.4 MB (76.09%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -3,15 +3,13 @@ class Solution(object):
         def reverseRow(row):
             l = 0
             r = len(row) - 1
-            while l < r:
-                row[l], row[r] = (row[r], row[l])
+            while l <= r:
+                row[l], row[r] = (1 - row[r], 1 - row[l])
                 l += 1
                 r -= 1
     
         for row in image:
             reverseRow(row)
-            for i in range(len(row)):
-                row[i] = 1 - row[i]
 
         return image
         
