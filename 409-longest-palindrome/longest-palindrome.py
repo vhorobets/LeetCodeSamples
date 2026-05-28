cac274eae19d80943b1107ac049e7b84a4c1fd4b
vhorobets <valikgor1@gmail.com>
2026-05-28 17:07:11 -0600

    Time: 1 ms (59.72%) | Memory: 19.3 MB (67.68%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -3,15 +3,15 @@ class Solution:
         counts = defaultdict(int)
 
         for c in s:
-            counts[c] = counts[c] + 1
+            counts[c] += 1
 
         result = 0
         has_odd = False
         for cnt in counts.values():
             if cnt % 2 == 0:
-                result = result + cnt
+                result += cnt
             else:
-                result = result + cnt - 1
+                result += cnt - 1
                 has_odd = True
 
         return result + 1 if has_odd else result
