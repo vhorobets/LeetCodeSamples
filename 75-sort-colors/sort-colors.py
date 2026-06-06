f3e7d802096c170661d4275ab0701aaf5e842333
vhorobets <valikgor1@gmail.com>
2026-06-06 13:33:32 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (61.50%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -11,12 +11,12 @@ class Solution:
             if nums[current] == 0:
                 nums[left], nums[current] = nums[current], nums[left]
                 left += 1
-                current += 1
             elif nums[current] == 2:
                 nums[right], nums[current] = nums[current], nums[right]
                 right -= 1
-            else:
-                current += 1
+                continue
+
+            current += 1
 
             
 
